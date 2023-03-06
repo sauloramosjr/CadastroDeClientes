56775cd9982d320845c71fbbe5324d4c2fb6aea7
Saulo Ramos <57292499+sauloramosjr@users.noreply.github.com>
2023-03-06 10:22:41 -0300

    Update Clientes.sql

--- Clientes.sql
+++ Clientes.sql
@@ -1,6 +1,11 @@
+-- Cria o banco de dados
+CREATE DATABASE DB_Cadastro_Clientes;
+-- Utiliza o banco de dados criado 
+USE DB_Cadastro_Clientes;
+-- cria a tabela de clientes
 CREATE TABLE Clientes (
-   Id INT PRIMARY KEY IDENTITY(1,1),
-   Nome VARCHAR(100) NOT NULL,
+   Codigo INT PRIMARY KEY IDENTITY(1,1),
+   Nome VARCHAR(50) NOT NULL,
    CNPJ VARCHAR(14) NOT NULL UNIQUE,
    DataCadastro DATE NOT NULL,
    Endereco VARCHAR(100) NOT NULL,
